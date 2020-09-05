e2a704cec6b2144236da19023e847e240a401112
PietAlex <pietalexventer@yahoo.com>
2020-09-05 22:09:53 +0200

    feat(p2p-assessment): Added navigation and banner area to project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SubnavComponent } from './subnav/subnav.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SubnavComponent,
+    BreadcrumbComponent,
+    HeroBannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
